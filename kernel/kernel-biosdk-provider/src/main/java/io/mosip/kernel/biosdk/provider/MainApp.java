389bf2f8f2dae7ba8db3013d4adaee1122df7437
Manoj SP <Manoj.SP@mindtree.com>
2020-08-05 21:09:06 +0530

    [MOSIP-8294] removed kernel-idgenerator-tokenid module

---
+++ kernel/kernel-biosdk-provider/src/main/java/io/mosip/kernel/biosdk/provider/MainApp.java
@@ -0,0 +1,13 @@
+package io.mosip.kernel.biosdk.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
